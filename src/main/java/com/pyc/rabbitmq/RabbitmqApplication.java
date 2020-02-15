cfd9b68cc58c81d395a26c41a272199ece08a0fe
ycy-ycy <2923616405@qq.com>
2020-02-15 15:19:56 +0800

    Update

--- src/main/java/com/pyc/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/pyc/rabbitmq/RabbitmqApplication.java
@@ -1,10 +1,32 @@
 package com.pyc.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-public class RabbitmqApplication {
+public class RabbitmqApplication implements CommandLineRunner {
+
+    // using rabbit template
+    @Autowired
+    RabbitTemplate template;
+
+    // define destination also called queue, named my-queue
+    @Bean
+    public Queue pycQueue(){
+        return new Queue("my-queue");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // using convertAndSend method to send message
+        template.convertAndSend("my-queue", "A greet message come from RabbitMQ");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqApplication.class, args);
